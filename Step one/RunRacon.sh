7a7f9843db32960878fb9cc0daa72d2bfd2d8835
burnsro <burnsro@tcd.ie>
2021-07-27 17:18:44 +0200

    Update RunRacon.sh

--- Step one/RunRacon.sh
+++ Step one/RunRacon.sh
@@ -4,58 +4,43 @@
 #SBATCH --mem=40G   # 64GB of RAM
 #SBATCH --qos=short
 #SBATCH --time=0-04:00:00
-#SBATCH --output=%A_%a.11B02Ragon.stdout
-# SBATCH --array=1-167
+#SBATCH --output=%A_%a.AqRagon.stdout
+#SBATCH --array=1-N 
 
-CONDA_ENVS_PATH='/groups/nordborg/projects/suecica/005scripts/001Software'
-CONDA_PKGS_DIRS=/opt/anaconda/pkgs='/groups/nordborg/projects/suecica/005scripts/001Software'
-#ml anaconda3/2019.03
-#conda create -p /groups/nordborg/projects/suecica/005scripts/001Software/RobinCondaSCRATCH python=2.7 #or 3.6 just run this command once then source it
-cd /groups/nordborg/projects/suecica/005scripts/001Software
+#### N in the batch job is how many fastq files was generated from Nanopore, change accordingly
+
+#have nglmr in your conda en
 ml anaconda3/2019.03
-source activate RobinCondaSCRATCH/
+source activate /groups/nordborg/projects/suecica/005scripts/001Software/RobinCondaSCRATCH/
 ml samtools/1.9-foss-2018b
 
-#out='/scratch-cbe/users/robin.burns/011Alyrata/11BO2_Assembly'
-#out='/scratch-cbe/users/robin.burns/007pbreads'
-#ref=$out'/11B02.contigs.fasta'
-#ref=$out/'Asue_genome.HiCGeneticMap.270919.fasta'
-#ref='/groups/nordborg/projects/suecica/001Assembly/004Asuecica/Asue_genome.HiCGeneticMap.270919.Popte2.fasta'
-#raw_reads='/groups/nordborg/projects/nordborg_rawdata/Alyrata/001Minion/11B02/20191009_0859_MN25531_FAL00802_39713699/fastq_pass'
-#mkdir /scratch-cbe/users/robin.burns/01011B02/pbreads_ref
-out='/scratch-cbe/users/robin.burns/001Alyrata/longreads'
-out2='/scratch-cbe/users/robin.burns/001Alyrata/longreads/11B02'
-raw_reads='/groups/nordborg/projects/nordborg_rawdata/Alyrata/001Minion/11B02/20191009_0859_MN25531_FAL00802_39713699/fastq_pass'
-ref=$out'/11B02_240920.fasta'
 
-samples=$raw_reads'/myfastq'
+out='/scratch-cbe/users/robin.burns/001Alyrata/longreads'
+
+raw_reads='/path/to/longreads/fastq' #minion puts fastq in fastq_pass folder
+ref=$out'/DLF21_Canu_contigs.fasta' #contigs, make sure it is indexed with samtools faidx
+
+samples=$raw_reads'/allmyfastq' #each fastq on a seperate line
 acc=$(awk "NR==$SLURM_ARRAY_TASK_ID" $samples)
 echo $acc
 
 
 ngmlr -t 16 -r $ref -q $raw_reads/$acc -o $out/${acc}.sam -x ont
-#mv $out/${acc}.sam $out2/.
-#samtools view -@ 16  -bh -t $ref.fai -o  $out2/${acc}.bam  $out2/${acc}.sam
-#samtools sort -@ 16 -o ${out2}/${acc}.sort.bam ${out2}/${acc}.bam
-#samtools index ${out2}/${acc}.sort.bam
+samtools view -@ 16  -bh -t $ref.fai -o  $out/${acc}.bam  $out/${acc}.sam
+samtools sort -@ 16 -o ${out}/${acc}.sort.bam ${out}/${acc}.bam
+samtools index ${out}/${acc}.sort.bam
 
-#ml bamtools/2.5.1-foss-2018b
-#cd $out2
-#ls *sort.bam > mybams
-#bamtools merge -list mybams -out 11B02.bamtoolsmerged.bam
-#cd $out
-#rm *.tmp*
-#samtools sort -@ 16 -o 11B02.bamtoolsmerged.sort.bam 11B02.bamtoolsmerged.bam
-#samtools index 11B02.bamtoolsmerged.sort.bam 
-#ASS3.bamtoolsmerged.bam
-#variantCaller --algorithm=arrow -j8 ${out}/arrow/11B02.bamtoolsmerged.sort.bam \
-# -r $out/11B02.contigs.fasta -o ${out}/11B02.contigs_arrow.gff    \
-# -o ${out}/11B02.contigs_arrow.fasta -o ${out}/11B02.contigs_arrow.fastq
+#####################Do next steps outside of batch job###########
+ml bamtools/2.5.1-foss-2018b
 
+ls ${out}/*sort.bam > mybams
+bamtools merge -list mybams -out DLF21.bamtoolsmerged.bam
 
-sniffles -m 11B02.bamtoolsmerged.sort.bam -v  11B02.sniffles.vcf --max_num_splits 2 -l 50 -t 8 -f 1.0
+samtools sort -@ 16 -o DLF21.bamtoolsmerged.sort.bam DLF21.bamtoolsmerged.bam
+samtools index DLF21.bamtoolsmerged.sort.bam 
 
-sniffles -m 11B02.bamtoolsmerged.sort.bam -b 11B02.sniffles.bed --max_num_splits 2 -l 50 -t 8 -n 10 
+cat ${raw_reads}/*fastq > $out/DLF21_all.fastq #put all fastq together 
+samtools view -h -o ${out}/DLF21.bamtoolsmerged.sort.bam ${out}/DLF21.bamtoolsmerged.sort.sam
+racon -t 16 ${raw_reads}/DLF21_all.fastq ${out}/DLF21.bamtoolsmerged.sort.sam ${ref} > $out/${ref}_racon.fasta #corrected contigs 
+rm $out/DLF21_all.fastq
 
-#samtools view -h -o ${out}/arrow/11B02.bamtoolsmerged.sort.sam ${out}/arrow/11B02.bamtoolsmerged.sort.bam
-#racon -t 8 ${raw_reads}/11B02_all.fastq ${out}/arrow/11B02.bamtoolsmerged.sort.sam $out/11B02.contigs.fasta > $out/11B02.contigs_racon.fasta 
