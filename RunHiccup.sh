927af9f15cfe568adc8a218947f08bb72092dd77
burnsro <burnsro@tcd.ie>
2021-07-27 14:13:54 +0200

    Update RunHiccup.sh

--- RunHiccup.sh
+++ RunHiccup.sh
@@ -23,3 +23,25 @@ hicup_digester --genome Aq_maskedDisgested --re1 ^GATC,DpnII ${Aqgenome}
 
 #Check configuration file and change paths accordingly of digestion file and index file and where the raw reads and the output will go
 hicup --config ${myconf} --threads 8
+
+#After running hiccup process sam file to visualize in juicer for rearrangements
+#In juicer you can make rearrangments manually and it will output a text file of what regions to change 
+#Can then run juicer script to make these changes to the fasta file
+#other tools for visualization also available (but juicer more interactive)
+
+
+cd $outdir
+#here each assembled scaffold is called "chr" followed by number
+sam=Aq.hiccup.sam
+preinput=Aq.hiccup.txt
+
+samtools view $sam | awk 'BEGIN {FS="\t"; OFS="\t"} {name1=substr($1,0,length($1)-2); str1=and($2,16); chr1=$3; pos1=$4; mapq1=$5; getline; name2=substr($1,0,length($1)-2); str2=and($2,16); chr2=$3; pos2=$4; mapq2=$5; if(name1==name2) { if (chr1>chr2){print name1, str2, "chr" chr2, pos2,1, str1, "chr" chr1, pos1, 0, mapq2, mapq1} else {print name1, str1, "chr" chr1, pos1, 0, str2, "chr" chr2, pos2 ,1, mapq1, mapq2}}}' | awk  '{gsub("\tchr\t","\thpv\t",$0); print;}' | sort -k3,3d -k7,7d > $preinput
+
+
+cat $preinput | sort -k3,3d -k7,7d -k4,4n -k8,8n > ${preinput}.sort.txt
+
+
+ml java/1.8.0_212
+
+java -Xmx80g -jar ${juicerdir}/juicer_tools_1.13.02.jar pre ${preinput}.sort.txt -r 5000,10000,20000,25000,50000 ${preinput}.hic /path/to/text/file/with/sizes/of/chromosomes
+#eg chr1 32000000
