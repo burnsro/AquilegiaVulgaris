42dc22b3eeccc5bd2f44bff7fc7e3701e1f47be0
burnsro <burnsro@tcd.ie>
2021-07-28 11:49:27 +0200

    Update RunJellyfish_1.sh

--- Step one/RunJellyfish_1.sh
+++ Step one/RunJellyfish_1.sh
@@ -17,6 +17,6 @@ source activate /groups/nordborg/projects/suecica/005scripts/001Software/RobinCo
 
 cd $out
 zcat ${raw}/${acc}.1.fastq.gz | jellyfish count /dev/fd/0 -C -o ${acc}_21mer -m 21 -t 16 -s 5G
-jellyfish histo -h 3000000 -o ${acc}test_21merhisto ${acc}_21mer
+jellyfish histo -h 3000000 -o ${acc}_21merhisto ${acc}_21mer
 
 
